ad13f24de39daa0e19d0183a98bbd1eb450b2a16
kingaboros <71248858+kingaboros@users.noreply.github.com>
2021-01-18 16:30:22 +0000

    Added social links

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,37 +1,16 @@
 import React from "react"
-import {
-  FaFacebookSquare,
-  FaLinkedin,
-  FaDribbbleSquare,
-  FaBehanceSquare,
-  FaTwitterSquare,
-} from "react-icons/fa"
+import { FaFacebookSquare, FaLinkedin } from "react-icons/fa"
 
 const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/kngboros",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 5,
-    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/kingaboros/",
   },
 ]
 const links = data.map(link => {
