921dc61f98cbb033e4e49f45499af60640a6b8ed
kingaboros <71248858+kingaboros@users.noreply.github.com>
2021-01-18 16:30:06 +0000

    Completed Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,16 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
+import Skills from "../components/Skills"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 export default () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <Hero />
+      <Skills />
+    </Layout>
+  )
 }
 // ...GatsbyImageSharpFluid
