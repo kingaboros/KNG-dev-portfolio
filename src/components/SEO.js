5454b7ddc428574b5fc2187bd74ac681f7f2d379
kingaboros <71248858+kingaboros@users.noreply.github.com>
2021-01-20 16:32:11 +0000

    Adding meta for twitter cards

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -30,6 +30,14 @@ const SEO = ({ title, description }) => {
     <Helmet htmlAtributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
+
+      {/*twitter cards */}
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={siteDesc} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   )
 }
