f7013a7494ce19be764ff1afc1a37857e509a7da
kingaboros <71248858+kingaboros@users.noreply.github.com>
2021-01-20 13:59:29 +0000

    Setting up the toggle functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,13 @@ import React from "react"
 import logo from "../assets/logo.png"
 import { FaAlignRight } from "react-icons/fa"
 import PageLinks from "../constants/links"
-const Navbar = () => {
+
+const Navbar = ({ toggleSidebar }) => {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <img className="logo" src={logo} alt="logo" />
-        <button type="button" className="toggle-btn">
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
           <FaAlignRight></FaAlignRight>
         </button>
       </div>
